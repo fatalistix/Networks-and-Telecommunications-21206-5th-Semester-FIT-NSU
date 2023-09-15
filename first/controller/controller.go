48ede5959ea99d5ab2c2212ec6bf8a82837ed943
fatalistix <slavabal11@outlook.com>
2023-09-15 18:33:56 +0700

    added Init mock-foo, added view, mocks for btns

--- first/controller/controller.go
+++ first/controller/controller.go
@@ -1,13 +1,30 @@
 package controller
 
-import "fyne.io/fyne/v2"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+
+	"networks/first/view"
+)
 
 type Controller struct {
-	window fyne.Window
+	startWindow *fyne.Container
+	listWindow  *fyne.Container
+	window      fyne.Window
 }
 
-func NewController(window fyne.Window) Controller {
+func NewController(window fyne.Window) *Controller {
 	controller := Controller{}
 	controller.window = window
-	return controller
+	return &controller
+}
+
+func (s *Controller) Init() {
+	s.startWindow = view.NewStartWindowView(func(ip string) {
+		fmt.Println(ip)
+	}, func(ip string) {
+		fmt.Println(ip)
+	})
+	s.window.SetContent(s.startWindow)
 }
