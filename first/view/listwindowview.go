e0db15843091f972cb2467609d4b61789c7ba75f
fatalistix <slavabal11@outlook.com>
2023-09-17 16:28:02 +0700

    added layouts and primary containment

--- first/view/listwindowview.go
+++ first/view/listwindowview.go
@@ -1,1 +1,44 @@
 package view
+
+import (
+	"strconv"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type ListWindowViewHandler struct {
+	cont       *fyne.Container
+	users      []string
+	ipToUserId map[string]int
+}
+
+func NewListWindowView(
+	myIp, myName string,
+	port int,
+	onBackButtonPressed func(),
+) *ListWindowViewHandler {
+	handler := ListWindowViewHandler{}
+	handler.users = make([]string, 0)
+	handler.ipToUserId = make(map[string]int)
+
+	topLabel := widget.NewLabel(myName + "#" + myIp + ":" + strconv.Itoa(port))
+	list := widget.NewList(func() int {
+		return len(handler.users)
+	}, func() fyne.CanvasObject {
+		return widget.NewLabel(strings.Repeat("1", 50))
+	}, func(lii widget.ListItemID, co fyne.CanvasObject) {
+		co.(*widget.Label).SetText(handler.users[lii])
+	})
+	backButton := widget.NewButton("Back", onBackButtonPressed)
+	backButton.Importance = widget.HighImportance
+
+	handler.cont = container.NewBorder(topLabel, backButton, nil, nil, list)
+	return &handler
+}
+
+func (s *ListWindowViewHandler) Container() *fyne.Container {
+	return s.cont
+}
