6096c3907dbfa7472e6a4f19388be9e113a7e933
fatalistix <slavabal11@outlook.com>
2023-09-21 09:42:37 +0700

    organized controller's functions

--- first/controller/controller.go
+++ first/controller/controller.go
@@ -11,7 +11,7 @@ import (
 	"networks/first/view"
 )
 
-const port int = 6969
+const numOfErrors int = 5
 
 type Controller struct {
 	startView        *fyne.Container
@@ -19,14 +19,58 @@ type Controller struct {
 	settingsView     *fyne.Container
 	errorViewHandler *view.ErrorViewHandler
 	window           fyne.Window
-
-	ip4Server *ip4.IP4B6FServer
+	ip4Server        *ip4.IP4B6FServer
+	bufferSize       int
+	buffer           []byte
 }
 
 func NewController(window fyne.Window) *Controller {
 	controller := Controller{}
 	controller.window = window
+	controller.bufferSize = 1024
+	controller.buffer = make([]byte, controller.bufferSize)
 	return &controller
+}
+
+func (s *Controller) handleError(err error) {
+	s.errorViewHandler.UpdateErrorDescription(err)
+	s.window.SetContent(s.errorViewHandler.Container())
+}
+
+func (s *Controller) ip4Pressed(ip, iden string) {
+	s.window.SetContent(s.listViewHandler.Container())
+	s.listViewHandler.UpdateTitle(ip, model.B6FPort, iden)
+	var err error
+	s.ip4Server, err = ip4.NewIP4B6FServer(ip, model.B6FPort, iden)
+	if err != nil {
+		s.handleError(err)
+	}
+
+	err = s.ip4Server.Start(
+		s.buffer,
+		model.B6FSendingTimeoutMs,
+		numOfErrors,
+		func(addr *net.UDPAddr, packet model.B6FPacket) {
+			switch packet.MessageType() {
+			case model.B6F_MT_Report:
+				{
+					s.listViewHandler.Add(addr.IP.String(), addr.Port, packet.Id())
+				}
+			case model.B6F_MT_Leave:
+				{
+					s.listViewHandler.Remove(addr.IP.String(), addr.Port)
+				}
+			}
+		},
+	)
+
+	if err != nil {
+		s.handleError(err)
+	}
+}
+
+func (s *Controller) ip16Pressed(ip, iden string) {
+	fmt.Println("MOCK: ", ip, " # ", iden)
 }
 
 func (s *Controller) Init() {
@@ -35,37 +79,14 @@ func (s *Controller) Init() {
 	})
 
 	s.startView = view.NewStartWindowView(func(ip, iden string) {
-		s.window.SetContent(s.listViewHandler.Container())
-		s.listViewHandler.UpdateTitle(ip, port, iden)
-		var err error
-		s.ip4Server, err = ip4.NewIP4B6FServer(ip, port, iden)
-		if err != nil {
-			s.window.SetContent(s.errorViewHandler.Container())
-		}
-
-		buffer := make([]byte, 1024)
-		err = s.ip4Server.Start(buffer, 500, 500, func(addr *net.UDPAddr, packet model.B6FPacket) {
-			switch packet.MessageType() {
-			case model.Report:
-				{
-					s.listViewHandler.Add(addr.IP.String(), addr.Port, packet.Id())
-				}
-			case model.Leave:
-				{
-					s.listViewHandler.Remove(addr.IP.String(), addr.Port)
-				}
-			}
-		})
-		if err != nil {
-			s.window.SetContent(s.errorViewHandler.Container())
-		}
-	}, func(ip, id string) {
-		fmt.Println(ip)
+		s.ip4Pressed(ip, iden)
+	}, func(ip, iden string) {
+		s.ip16Pressed(ip, iden)
 	}, func() {
 		s.window.SetContent(s.settingsView)
 	})
 
-	s.listViewHandler = view.NewListWindowView("MOCK", "MOCK", port, func() {
+	s.listViewHandler = view.NewListWindowView("MOCK", "MOCK", model.B6FPort, func() {
 		s.window.SetContent(s.startView)
 		s.ip4Server.Close()
 	})
