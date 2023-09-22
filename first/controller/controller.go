a4f46e16cde0a1cf2e31360da378175f6666d6e1
fatalistix <slavabal11@outlook.com>
2023-09-22 23:32:36 +0700

    new ifaces adapted, added returns (avoid nil ptr)

--- first/controller/controller.go
+++ first/controller/controller.go
@@ -1,14 +1,15 @@
 package controller
 
 import (
-	"fmt"
 	"net"
 	"strconv"
+	"strings"
 	"time"
 
 	"fyne.io/fyne/v2"
 
 	"networks/first/model"
+	"networks/first/model/ip16"
 	"networks/first/model/ip4"
 	"networks/first/view"
 )
@@ -26,7 +27,7 @@ type Controller struct {
 	settingsView     *fyne.Container
 	errorViewHandler *view.ErrorViewHandler
 	window           fyne.Window
-	ip4Server        *ip4.IP4B6FServer
+	server           *model.B6FServer
 	bufferSize       int
 	buffer           []byte
 	updaterClose     chan bool
@@ -90,16 +91,20 @@ func (s *Controller) createUpdater() {
 	}()
 }
 
-func (s *Controller) ip4Pressed(ip, iden string) {
+func (s *Controller) startServer(
+	ip, iden string,
+	multicaster model.Multicaster,
+	listener model.MulticastListener,
+) {
 	s.window.SetContent(s.listViewHandler.Container())
-	s.listViewHandler.UpdateTitle(ip+":"+strconv.Itoa(model.B6FPort), iden)
+	s.listViewHandler.UpdateTitle(strings.ToUpper(ip)+":"+strconv.Itoa(model.B6FPort), iden)
 	var err error
-	s.ip4Server, err = ip4.NewIP4B6FServer(ip, model.B6FPort, iden)
+	s.server, err = model.NewB6FServer(ip, model.B6FPort, iden, multicaster, listener)
 	if err != nil {
 		s.handleError(err)
+		return
 	}
-
-	err = s.ip4Server.Start(
+	err = s.server.Start(
 		s.buffer,
 		model.B6FSendingTimeoutMs,
 		numOfErrors,
@@ -110,11 +115,16 @@ func (s *Controller) ip4Pressed(ip, iden string) {
 
 	if err != nil {
 		s.handleError(err)
+		return
 	}
 }
 
+func (s *Controller) ip4Pressed(ip, iden string) {
+	s.startServer(ip, iden, ip4.NewIP4Multicaster(), ip4.NewIP4MulticastListener())
+}
+
 func (s *Controller) ip16Pressed(ip, iden string) {
-	fmt.Println("MOCK: ", ip, " # ", iden)
+	s.startServer(ip, iden, ip16.NewIP16Multicaster(), ip16.NewIP16MulticastListener())
 }
 
 func (s *Controller) Init() {
@@ -126,7 +136,7 @@ func (s *Controller) Init() {
 		s.createUpdater()
 		s.ip4Pressed(ip, iden)
 	}, func(ip, iden string) {
-		// s.createUpdater()
+		s.createUpdater()
 		s.ip16Pressed(ip, iden)
 	}, func() {
 		s.window.SetContent(s.settingsView)
@@ -134,7 +144,7 @@ func (s *Controller) Init() {
 
 	s.listViewHandler = view.NewListWindowView("MOCK", "MOCK", model.B6FPort, func() {
 		s.window.SetContent(s.startView)
-		s.ip4Server.Close()
+		s.server.Close()
 		s.updaterClose <- true
 		s.listViewHandler.ClearList()
 	})
