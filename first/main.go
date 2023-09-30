9079c21bd9ad5d5e675752267bb3c8ea486482c3
fatalistix <slavabal11@outlook.com>
2023-09-30 22:25:41 +0700

    removed comments after main func

--- first/main.go
+++ first/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-
 	"networks/first/controller"
+
+	"fyne.io/fyne/v2/app"
 )
 
 func main() {
@@ -13,30 +13,3 @@ func main() {
 	cntrlr.Init()
 	window.ShowAndRun()
 }
-
-// func main() {
-// 	ur, err := reciever.Connect("235.0.0.1:6969")
-// 	fmt.Println(ur, err)
-// 	buffer := make([]byte, 5)
-// 	ur.Recieve(buffer)
-// 	fmt.Println(buffer)
-// }
-
-// addr, _ := net.ResolveUDPAddr("udp", "localhost:8887")
-// conn, err := net.ListenUDP("udp", addr)
-//
-// if err != nil {
-// 	fmt.Println(err)
-// 	return
-// }
-// fmt.Println(addr)
-// fmt.Println(conn)
-//
-// defer conn.Close()
-//
-// buffer := make([]byte, 256)
-// n, raddr, _ := conn.ReadFromUDP(buffer)
-//
-// fmt.Println(n)
-// fmt.Println(raddr)
-// fmt.Println(strings.Trim(string(buffer), " "))
