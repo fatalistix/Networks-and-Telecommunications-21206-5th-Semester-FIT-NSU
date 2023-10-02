d9a0319b823543e35bca765d5d593352cb3c3bf0
fatalistix <slavabal11@outlook.com>
2023-10-02 13:13:46 +0700

    swapped file asking with connection asking

--- second/Client/app/src/main/java/ru/nsu/vbalashov2/tcpfileclient/App.java
+++ second/Client/app/src/main/java/ru/nsu/vbalashov2/tcpfileclient/App.java
@@ -8,6 +8,19 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.print("Input path to file: ");
+    String pathToFile = scanner.next();
+    scanner.nextLine();
+    Path path = Path.of(pathToFile);
+    if (!Files.exists(path)) {
+      System.out.println("File does not exists");
+      return;
+    }
+    if (Files.isDirectory(path)) {
+      System.out.println("Expected path to file, got path to directory");
+      return;
+    }
+
     System.out.print("Input server addres: ");
     String serverAddr = scanner.next();
     scanner.nextLine();
@@ -19,26 +32,6 @@ public class App {
       client.connect(serverAddr, port);
     } catch (IOException e) {
       System.out.println("Error connecting: " + e.getMessage());
-      try {
-        client.close();
-      } catch (IOException ignored) {
-      }
-      return;
-    }
-    System.out.print("Input path to file: ");
-    String pathToFile = scanner.next();
-    scanner.nextLine();
-    Path path = Path.of(pathToFile);
-    if (!Files.exists(path)) {
-      System.out.println("File does not exists");
-      try {
-        client.close();
-      } catch (IOException ignored) {
-      }
-      return;
-    }
-    if (Files.isDirectory(path)) {
-      System.out.println("Expected path to file, got path to directory");
       try {
         client.close();
       } catch (IOException ignored) {
