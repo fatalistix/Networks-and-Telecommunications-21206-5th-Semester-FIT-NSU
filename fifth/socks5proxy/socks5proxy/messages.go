a9b7bc6c168bf1aeb2ad4376cfbe5911d5de6a78
fatalistix <slavabal11@outlook.com>
2023-10-31 20:10:40 +0700

    replaced byte fixed proto values with iota

--- fifth/socks5proxy/socks5proxy/messages.go
+++ fifth/socks5proxy/socks5proxy/messages.go
@@ -1,59 +1,84 @@
 package socks5proxy
 
-//type socksVersion byte
-//
-//const (
-//	socks5 socksVersion = iota + 5
-//)
+type socksVersion byte
+
+const (
+	socks5 socksVersion = iota + 5
+)
+
+func (s socksVersion) GetByte() byte {
+	return byte(s)
+}
 
 type socks5GreetingMessage struct {
-	SocksVersion     byte
+	SocksVersion     socksVersion
 	NumOfAuthMethods byte
 	AuthMethods      []byte
 }
 
 type socks5GreetingAnswer struct {
-	SocksVersion byte
+	SocksVersion socksVersion
 	AuthMethod   byte
 }
 
-//type messageCode byte
-//
-//const (
-//	_ messageCode = iota
-//	establishTCP
-//	bindPort
-//	associateUDPPort
-//)
+type messageCode byte
 
-//type addressType byte
-//
-//const (
-//	_ addressType = iota
-//	ipv4
-//	_
-//	domainName
-//	ipv6
-//)
+const (
+	_ messageCode = iota
+	establishTCP
+	bindPort
+	associateUDPPort
+)
+
+func (s messageCode) GetByte() byte {
+	return byte(s)
+}
+
+type addressType byte
+
+const (
+	_ addressType = iota
+	ipv4
+	_
+	domainName
+	ipv6
+)
+
+func (s addressType) GetByte() byte {
+	return byte(s)
+}
 
 type socks5ClientMessage struct {
-	SocksVersion   byte
-	MessageCode    byte
-	AddressType    byte
+	SocksVersion   socksVersion
+	MessageCode    messageCode
+	AddressType    addressType
 	AddressPayload []byte
 	Port           uint16
 }
 
-//type answerCode byte
-//
-//const (
-//	requestGranted answerCode = iota
-//)
+type answerCode byte
+
+const (
+	requestGranted answerCode = iota
+	generalFailure
+	notAllowedByRuleset
+	networkUnreachable
+	hostUnreachable
+	connectionRefusedByDestinationHost
+	ttlExpired
+	commandNotSupported
+	protocolError = iota - 1
+	addressTypeNotSupported
+)
+
+func (s answerCode) GetByte() byte {
+	return byte(s)
+}
 
 type socks5ServerAnswer struct {
-	SocksVersion   byte
-	AnswerCode     byte
-	AddressType    byte
+	SocksVersion   socksVersion
+	AnswerCode     answerCode
+	AddressType    addressType
 	AddressPayload []byte
 	Port           uint16
 }
