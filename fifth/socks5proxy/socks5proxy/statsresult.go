5f7eb87205b432f7f8c0c2d83513f9447bcb9bca
fatalistix <slavabal11@outlook.com>
2023-11-01 13:21:55 +0700

    result stract of stats collection

--- fifth/socks5proxy/socks5proxy/statsresult.go
+++ fifth/socks5proxy/socks5proxy/statsresult.go
@@ -1,1 +1,17 @@
 package socks5proxy
+
+import (
+	"net"
+	"time"
+)
+
+type StatsResult struct {
+	ReadBytes           uint64
+	WroteBytes          uint64
+	ReadSinceLastStats  uint64
+	WroteSinceLastStats uint64
+	StartTime           time.Time
+	LastStatsTime       time.Time
+	IP                  net.IP
+	Port                uint16
+}
