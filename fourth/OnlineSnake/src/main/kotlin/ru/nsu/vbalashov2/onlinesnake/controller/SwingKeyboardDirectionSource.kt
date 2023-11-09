53d36f162280aaa9e553e37f2211c4bb12d950ec
fatalistix <slavabal11@outlook.com>
2023-11-09 16:41:11 +0700

    swing specific keyboard keys handler for setting snake's head direction

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/controller/SwingKeyboardDirectionSource.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/controller/SwingKeyboardDirectionSource.kt
@@ -1,24 +1,42 @@
-package ru.nsu.vbalashov2.onlinesnake.directionsource.impl
+package ru.nsu.vbalashov2.onlinesnake.controller
 
 import ru.nsu.vbalashov2.onlinesnake.model.DirectionSource
-import ru.nsu.vbalashov2.onlinesnake.model.gamefield.Direction
+import ru.nsu.vbalashov2.onlinesnake.model.Direction
+import java.awt.KeyboardFocusManager
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 
-class SwingKeyboardDirectionSource(initDirection: Direction) : KeyAdapter(), DirectionSource {
+class SwingKeyboardDirectionSource(override val initDirection: Direction) : DirectionSource /*, KeyAdapter() */ {
     private var _direction = initDirection
 
     override val direction: Direction
         get() = _direction
 
-    override fun keyPressed(e: KeyEvent?) {
-        val keyCode = e!!.keyCode
-        _direction = when (keyCode) {
-            KeyEvent.VK_LEFT, KeyEvent.VK_A -> Direction.LEFT
-            KeyEvent.VK_DOWN, KeyEvent.VK_S -> Direction.DOWN
-            KeyEvent.VK_RIGHT, KeyEvent.VK_D -> Direction.RIGHT
-            KeyEvent.VK_UP, KeyEvent.VK_W -> Direction.UP
-            else -> _direction
-        }
+
+    init {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager()
+            .addKeyEventDispatcher {
+                val keyCode = it!!.keyCode
+                _direction = when (keyCode) {
+                    KeyEvent.VK_LEFT, KeyEvent.VK_A -> Direction.LEFT
+                    KeyEvent.VK_DOWN, KeyEvent.VK_S -> Direction.DOWN
+                    KeyEvent.VK_RIGHT, KeyEvent.VK_D -> Direction.RIGHT
+                    KeyEvent.VK_UP, KeyEvent.VK_W -> Direction.UP
+                    else -> _direction
+                }
+                false
+            }
     }
+//
+//    override fun keyPressed(e: KeyEvent?) {
+//        val keyCode = e!!.keyCode
+//        _direction = when (keyCode) {
+//            KeyEvent.VK_LEFT, KeyEvent.VK_A -> Direction.LEFT
+//            KeyEvent.VK_DOWN, KeyEvent.VK_S -> Direction.DOWN
+//            KeyEvent.VK_RIGHT, KeyEvent.VK_D -> Direction.RIGHT
+//            KeyEvent.VK_UP, KeyEvent.VK_W -> Direction.UP
+//            else -> _direction
+//        }
+//        println(_direction)
+//    }
 }
