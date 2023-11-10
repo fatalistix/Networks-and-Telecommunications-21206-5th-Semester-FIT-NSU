00c47723179edec4a1354be6f383974ac125aba2
fatalistix <slavabal11@outlook.com>
2023-11-10 16:23:08 +0700

    moved buttons to one panel

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ButtonsPanel.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ButtonsPanel.kt
@@ -5,20 +5,36 @@ import java.awt.GridBagLayout
 import javax.swing.JButton
 import javax.swing.JPanel
 
-class NewGamePanel : JPanel() {
+class ButtonsPanel : JPanel() {
+    private val exitButton = JButton("Exit")
     private val newGameButton = JButton("New")
 
     init {
         this.layout = GridBagLayout()
+    }
 
+    init {
+        val gbcExitButton = GridBagConstraints()
+        gbcExitButton.gridx = 0
+        gbcExitButton.gridy = 0
+        gbcExitButton.gridwidth = 1
+        gbcExitButton.gridheight = 1
+        gbcExitButton.fill = GridBagConstraints.BOTH
+        gbcExitButton.anchor = GridBagConstraints.NORTHWEST
+        gbcExitButton.weightx = 50.0
+        gbcExitButton.weighty = 100.0
+        this.add(exitButton, gbcExitButton)
+    }
+
+    init {
         val gbcNewGameButton = GridBagConstraints()
-        gbcNewGameButton.gridx = 0
+        gbcNewGameButton.gridx = 1
         gbcNewGameButton.gridy = 0
         gbcNewGameButton.gridwidth = 1
         gbcNewGameButton.gridheight = 1
         gbcNewGameButton.fill = GridBagConstraints.BOTH
         gbcNewGameButton.anchor = GridBagConstraints.NORTHWEST
-        gbcNewGameButton.weightx = 100.0
+        gbcNewGameButton.weightx = 500.0
         gbcNewGameButton.weighty = 100.0
         this.add(newGameButton, gbcNewGameButton)
     }
