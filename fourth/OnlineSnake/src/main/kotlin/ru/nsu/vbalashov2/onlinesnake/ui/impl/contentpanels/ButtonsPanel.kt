1418c893b94757dae075f464c9f7864ce346f700
fatalistix <slavabal11@outlook.com>
2023-11-10 21:31:20 +0700

    replaced GridBagLayout with GridLayout

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ButtonsPanel.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ButtonsPanel.kt
@@ -2,6 +2,7 @@ package ru.nsu.vbalashov2.onlinesnake.ui.impl.contentpanels
 
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
+import java.awt.GridLayout
 import javax.swing.JButton
 import javax.swing.JPanel
 
@@ -10,32 +11,41 @@ class ButtonsPanel : JPanel() {
     private val newGameButton = JButton("New")
 
     init {
-        this.layout = GridBagLayout()
+        this.layout = GridLayout(1, 2)
     }
 
     init {
-        val gbcExitButton = GridBagConstraints()
-        gbcExitButton.gridx = 0
-        gbcExitButton.gridy = 0
-        gbcExitButton.gridwidth = 1
-        gbcExitButton.gridheight = 1
-        gbcExitButton.fill = GridBagConstraints.BOTH
-        gbcExitButton.anchor = GridBagConstraints.NORTHWEST
-        gbcExitButton.weightx = 50.0
-        gbcExitButton.weighty = 100.0
-        this.add(exitButton, gbcExitButton)
+        this.add(exitButton)
+        this.add(newGameButton)
     }
 
-    init {
-        val gbcNewGameButton = GridBagConstraints()
-        gbcNewGameButton.gridx = 1
-        gbcNewGameButton.gridy = 0
-        gbcNewGameButton.gridwidth = 1
-        gbcNewGameButton.gridheight = 1
-        gbcNewGameButton.fill = GridBagConstraints.BOTH
-        gbcNewGameButton.anchor = GridBagConstraints.NORTHWEST
-        gbcNewGameButton.weightx = 500.0
-        gbcNewGameButton.weighty = 100.0
-        this.add(newGameButton, gbcNewGameButton)
-    }
+//    init {
+//        this.layout = GridBagLayout()
+//    }
+//
+//    init {
+//        val gbcExitButton = GridBagConstraints()
+//        gbcExitButton.gridx = 0
+//        gbcExitButton.gridy = 0
+//        gbcExitButton.gridwidth = 1
+//        gbcExitButton.gridheight = 1
+//        gbcExitButton.fill = GridBagConstraints.BOTH
+//        gbcExitButton.anchor = GridBagConstraints.NORTHWEST
+//        gbcExitButton.weightx = 50.0
+//        gbcExitButton.weighty = 100.0
+//        this.add(exitButton, gbcExitButton)
+//    }
+//
+//    init {
+//        val gbcNewGameButton = GridBagConstraints()
+//        gbcNewGameButton.gridx = 1
+//        gbcNewGameButton.gridy = 0
+//        gbcNewGameButton.gridwidth = 1
+//        gbcNewGameButton.gridheight = 1
+//        gbcNewGameButton.fill = GridBagConstraints.BOTH
+//        gbcNewGameButton.anchor = GridBagConstraints.NORTHWEST
+//        gbcNewGameButton.weightx = 500.0
+//        gbcNewGameButton.weighty = 100.0
+//        this.add(newGameButton, gbcNewGameButton)
+//    }
 }
