2517a402a8197186f62a2a243b2af46c58a5cb0a
fatalistix <slavabal11@outlook.com>
2023-11-11 23:07:13 +0700

    added announcement sending method

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/MessageBed.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/MessageBed.kt
@@ -1,4 +1,7 @@
 package ru.nsu.vbalashov2.onlinesnake.net
 
+import ru.nsu.vbalashov2.onlinesnake.net.dto.MsgAnnouncement
+
 interface MessageBed {
+    fun writeAnnouncement(msgAnnouncement: MsgAnnouncement)
 }
