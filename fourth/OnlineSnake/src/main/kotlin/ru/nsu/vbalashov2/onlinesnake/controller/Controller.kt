c809d6fd815e861ee5a6f710dac422a1af62115b
fatalistix <slavabal11@outlook.com>
2023-11-11 22:09:01 +0700

    listens to new game and exit events

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/controller/Controller.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/controller/Controller.kt
@@ -2,28 +2,47 @@ package ru.nsu.vbalashov2.onlinesnake.controller
 
 import ru.nsu.vbalashov2.onlinesnake.model.SnakeGame
 import ru.nsu.vbalashov2.onlinesnake.model.SnakeKey
-import ru.nsu.vbalashov2.onlinesnake.ui.GameUI
+import ru.nsu.vbalashov2.onlinesnake.ui.ExitListener
+import ru.nsu.vbalashov2.onlinesnake.ui.NewGameListener
+import ru.nsu.vbalashov2.onlinesnake.ui.dto.GameConfig
 import ru.nsu.vbalashov2.onlinesnake.ui.impl.GameUIPanel
 import javax.swing.JFrame
 import javax.swing.SwingUtilities
 
 
-class Controller(private val mainAppFrame: JFrame) {
+class Controller(private val mainAppFrame: JFrame) : NewGameListener, ExitListener {
     private val gameUI = GameUIPanel()
     private val players: MutableMap<Int, SnakeKey> = mutableMapOf()
+    private var game: SnakeGame? = null
 
     init {
         this.mainAppFrame.contentPane.add(gameUI)
     }
 
-    private val game = SnakeGame(10, 10, 1_000) { snakes, fieldArray ->
-        SwingUtilities.invokeLater {
-            gameUI.updateField(fieldArray, 10, 10)
-        }
+    init {
+        gameUI.addNewGameListener(this)
+        gameUI.addExitListener(this)
     }
 
-    init {
-        val key = game.createSnake(SwingKeyboardDirectionSourceCreator())
-        players[0] = key
+    override fun newGame(gameConfig: GameConfig) {
+        game = SnakeGame(
+            width = gameConfig.width,
+            height = gameConfig.height,
+            foodStatic = gameConfig.foodStatic,
+            stateDelayMs = gameConfig.stateDelayMs,
+            onFieldUpdate = { _, fieldArray ->
+                SwingUtilities.invokeLater {
+                    gameUI.updateField(fieldArray, gameConfig.width, gameConfig.height)
+                }
+            }
+        )
+    }
+
+    override fun exit() {
+        if (game == null) {
+            return
+        }
+        game?.close()
+        game = null
     }
 }
