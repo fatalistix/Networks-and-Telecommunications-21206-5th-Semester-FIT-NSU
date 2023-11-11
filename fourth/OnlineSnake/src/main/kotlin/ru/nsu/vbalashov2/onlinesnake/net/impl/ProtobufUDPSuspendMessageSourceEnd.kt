efcd582e38e5581b3bdc0c5d1188638b64d1c27e
fatalistix <slavabal11@outlook.com>
2023-11-11 23:47:35 +0700

    Combined 2 class into 1 with joining ifaces

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/impl/ProtobufUDPSuspendMessageSourceEnd.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/impl/ProtobufUDPSuspendMessageSourceEnd.kt
@@ -6,11 +6,13 @@ import io.ktor.util.network.*
 import io.ktor.utils.io.core.*
 import kotlinx.coroutines.Dispatchers
 import ru.nsu.vbalashov2.onlinesnake.net.RawMessage
+import ru.nsu.vbalashov2.onlinesnake.net.SuspendMessageEnd
 import ru.nsu.vbalashov2.onlinesnake.net.SuspendMessageSource
+import ru.nsu.vbalashov2.onlinesnake.net.dto.MsgAnnouncement
 import ru.nsu.vbalashov2.onlinesnake.net.dto.common.SourceHost
 import ru.nsu.vbalashov2.onlinesnake.proto.OnlineSnakeProto
 
-class ProtobufUDPSuspendMessageSource(ip: String, port: Int) : SuspendMessageSource {
+class ProtobufUDPSuspendMessageSourceEnd(ip: String, port: Int) : SuspendMessageSource, SuspendMessageEnd {
     private val selectorManager = SelectorManager(Dispatchers.IO)
     private val socket = aSocket(selectorManager).udp().bind(InetSocketAddress(ip, port))
 
@@ -23,5 +25,9 @@ class ProtobufUDPSuspendMessageSource(ip: String, port: Int) : SuspendMessageSou
         println("address = " + datagram.address.toJavaAddress().address)
         println("hostname = " + datagram.address.toJavaAddress().hostname)
         return ProtobufRawMessage(OnlineSnakeProto.GameMessage.parseFrom(datagram.packet.readBytes())!!, sourceHost)
+    }
+
+    override suspend fun writeAnnouncement(msgAnnouncement: MsgAnnouncement) {
+
     }
 }
