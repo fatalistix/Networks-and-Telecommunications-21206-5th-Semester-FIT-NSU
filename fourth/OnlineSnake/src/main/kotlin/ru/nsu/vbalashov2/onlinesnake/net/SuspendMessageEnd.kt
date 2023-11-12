0e8c57fb0834fbb527b142a1e4120a17e631d364
fatalistix <slavabal11@outlook.com>
2023-11-12 13:57:18 +0700

    new message write opoportunity

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/SuspendMessageEnd.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/SuspendMessageEnd.kt
@@ -1,7 +1,9 @@
 package ru.nsu.vbalashov2.onlinesnake.net
 
 import ru.nsu.vbalashov2.onlinesnake.net.dto.MsgAnnouncement
+import ru.nsu.vbalashov2.onlinesnake.net.dto.MsgPing
 
 interface SuspendMessageEnd {
     suspend fun writeAnnouncement(msgAnnouncement: MsgAnnouncement)
+    suspend fun writePing(msgPing: MsgPing)
 }
