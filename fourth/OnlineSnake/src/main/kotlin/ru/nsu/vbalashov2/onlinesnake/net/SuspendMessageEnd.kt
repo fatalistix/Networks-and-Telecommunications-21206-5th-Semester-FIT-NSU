8943267429f9236a0371281a1dc2f36735e99626
fatalistix <slavabal11@outlook.com>
2023-11-19 18:55:33 +0700

    more message sending methods

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/SuspendMessageEnd.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/SuspendMessageEnd.kt
@@ -1,9 +1,12 @@
 package ru.nsu.vbalashov2.onlinesnake.net
 
-import ru.nsu.vbalashov2.onlinesnake.net.dto.MsgAnnouncement
-import ru.nsu.vbalashov2.onlinesnake.net.dto.MsgPing
+import ru.nsu.vbalashov2.onlinesnake.net.dto.*
 
 interface SuspendMessageEnd {
     suspend fun writeAnnouncement(msgAnnouncement: MsgAnnouncement)
     suspend fun writePing(msgPing: MsgPing)
+    suspend fun writeSteer(msgSteer: MsgSteer)
+    suspend fun writeJoin(msgJoin: MsgJoin)
+    suspend fun writeRoleChange(msgRoleChange: MsgRoleChange)
+    suspend fun writeError(msgError: MsgError)
 }
