b39b401734ef5e4ebb2db01f819d05357d93cf1f
fatalistix <slavabal11@outlook.com>
2023-11-19 18:53:04 +0700

    organized imports

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/OnlineSnakeApp.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/OnlineSnakeApp.kt
@@ -1,22 +1,7 @@
 package ru.nsu.vbalashov2.onlinesnake
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import ru.nsu.vbalashov2.onlinesnake.controller.Controller
-import javax.swing.SwingUtilities
-import javax.swing.UIManager
-
-//fun main() {
-//    runBlocking(Dispatchers.Main) {
-//        System.setProperty("sun.java2d.opengl", "true")
-//        SwingUtilities.invokeLater {
-//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-//            val frame = OnlineSnakeApp()
-//            Controller(frame)
-//            frame.isVisible = true
-//        }
-//    }
-//}
 
 fun main() =
     runBlocking {
