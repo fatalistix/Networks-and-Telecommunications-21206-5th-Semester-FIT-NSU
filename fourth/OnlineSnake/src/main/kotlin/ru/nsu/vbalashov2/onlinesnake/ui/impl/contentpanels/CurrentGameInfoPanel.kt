2501f5c9e9ac7a6f9c4dc4fae9e861be0439e419
fatalistix <slavabal11@outlook.com>
2023-12-16 00:39:01 +0700

    added labels prefixes and updating info methods

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/CurrentGameInfoPanel.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/CurrentGameInfoPanel.kt
@@ -5,13 +5,28 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 
 class CurrentGameInfoPanel : JPanel() {
-    private val ownerLabel = JLabel("Owner: ")
-    private val sizeLabel = JLabel("Field size: ")
-    private val foodLabel = JLabel("Food: ")
+    private val ownerPrefix = "Owner: "
+    private val sizePrefix = "Field size: "
+    private val foodPrefix = "Food on field: "
+    private val ownerLabel = JLabel()
+    private val sizeLabel = JLabel()
+    private val foodLabel = JLabel()
     init {
         this.layout = GridLayout(1, 3)
         this.add(ownerLabel)
         this.add(sizeLabel)
         this.add(foodLabel)
+    }
+
+    fun updateCurrentGameInfo(fieldWidth: Int, fieldHeight: Int, foodOnField: Int, ownerName: String) {
+        ownerLabel.text = ownerPrefix + ownerName
+        sizeLabel.text = sizePrefix + "${fieldWidth}x${fieldHeight}"
+        foodLabel.text = foodPrefix + foodOnField
+    }
+
+    fun clearCurrentGameInfo() {
+        ownerLabel.text = ""
+        sizeLabel.text = ""
+        foodLabel.text = ""
     }
 }
