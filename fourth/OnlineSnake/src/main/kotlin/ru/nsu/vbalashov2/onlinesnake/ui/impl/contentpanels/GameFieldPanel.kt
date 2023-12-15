6bf09e24a0725e16663d4b17560323c1399a8e65
fatalistix <slavabal11@outlook.com>
2023-12-16 00:39:53 +0700

    changed drawing logic and update field method signature

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/GameFieldPanel.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/GameFieldPanel.kt
@@ -1,65 +1,25 @@
 package ru.nsu.vbalashov2.onlinesnake.ui.impl.contentpanels
 
-import ru.nsu.vbalashov2.onlinesnake.ui.dto.KeyPoint
+import ru.nsu.vbalashov2.onlinesnake.dto.Coord
+import ru.nsu.vbalashov2.onlinesnake.dto.Snake
+import ru.nsu.vbalashov2.onlinesnake.dto.SnakeState
 import java.awt.*
 import javax.swing.JPanel
 import kotlin.math.absoluteValue
-import kotlin.math.max
 import kotlin.math.roundToInt
 import kotlin.math.sign
 
 class GameFieldPanel : JPanel() {
-    private var field = IntArray(1) { 0 }
     private var fieldWidth: Int = 1
     private var fieldHeight: Int = 1
-    private val snakeColors = arrayOf(Color.BLACK, Color.BLUE, Color.CYAN, Color.RED, Color.ORANGE, Color.YELLOW, Color.MAGENTA, Color.PINK, Color.LIGHT_GRAY)
+    private var myID: Int = 0
+//    private val snakeColors = arrayListOf(Color.BLACK, Color.BLUE, Color.CYAN, Color.RED, Color.ORANGE, Color.YELLOW, Color.MAGENTA, Color.PINK, Color.LIGHT_GRAY)
     private val foodColor = Color.GREEN
-    private var snakesKeyPointsList: List<List<KeyPoint>> = listOf()
-    private var foodList: List<KeyPoint> = listOf()
-//    private inner class ResizeListener : ComponentAdapter() {
-//        override fun componentResized(e: ComponentEvent?) {
-//            super.componentResized(e)
-//            size = if (height.toDouble() * fieldWidth > width.toDouble() * fieldHeight) {
-//                Dimension(width, (width.toDouble() / fieldWidth * fieldHeight).roundToInt())
-//            } else {
-//                Dimension((height.toDouble() / fieldHeight * fieldWidth).roundToInt(), height)
-//            }
-//        }
-//    }
-//    init {
-//        this.addComponentListener(ResizeListener())
-//    }
-
-//    override fun paint(g: Graphics?) {
-//        super.paint(g)
-//        val centerWidth: Boolean
-//        val fieldSizeDimension = if (height.toDouble() * fieldWidth > width.toDouble() * fieldHeight) {
-//            centerWidth = false
-//            Dimension(width, (width.toDouble() / fieldWidth * fieldHeight).roundToInt())
-//        } else {
-//            centerWidth = true
-//            Dimension((height.toDouble() / fieldHeight * fieldWidth).roundToInt(), height)
-//        }
-//
-//        val offset = if (centerWidth) {
-//            Dimension((width - fieldSizeDimension.width) / 2, 0)
-//        } else {
-//            Dimension(0, (height - fieldSizeDimension.height) / 2)
-//        }
-//
-//        val brickWidth = fieldSizeDimension.width.toDouble() / this.fieldWidth
-//        val brickHeight = fieldSizeDimension.height.toDouble() / this.fieldHeight
-//        for (i in 0..<fieldHeight) {
-//            for (j in 0..<fieldWidth) {
-//                if (field[i * fieldWidth + j] == -1) {
-//                    g?.color = foodColor
-//                } else {
-//                    g?.color = snakeColors[field[i * fieldWidth + j]]
-//                }
-//                g?.fillRect(offset.width + (j * brickWidth).roundToInt(), offset.height + (i * brickHeight).roundToInt(), brickWidth.roundToInt(), brickHeight.roundToInt())
-//            }
-//        }
-//    }
+    private val myColor = Color.YELLOW
+    private val aliveEnemyColor = Color.RED
+    private val zombieEnemyColor = Color.DARK_GRAY
+    private var snakesList: List<Snake> = listOf()
+    private var foodList: List<Coord> = listOf()
 
     override fun paint(g : Graphics?) {
         val centerWidth: Boolean
@@ -96,20 +56,31 @@ class GameFieldPanel : JPanel() {
             )
         }
 
-        for (snakeIndex in snakesKeyPointsList.indices) {
-            val snake = snakesKeyPointsList[snakeIndex]
-            var cx = snake.first().x
-            var cy = snake.first().y
-            g?.color = snakeColors[snakeIndex]
+        for (snakeIndex in snakesList.indices) {
+            val snake = snakesList[snakeIndex]
+            var cx = snake.pointList.first().x
+            var cy = snake.pointList.first().y
+            g?.color = when (snake.snakeState) {
+                SnakeState.ALIVE -> {
+                    if (snake.playerID == myID) {
+                        myColor
+                    } else {
+                        aliveEnemyColor
+                    }
+                }
+                SnakeState.ZOMBIE -> {
+                    zombieEnemyColor
+                }
+            }
             g?.fillRect(
                 offset.width + (cx * brickWidth).roundToInt(),
                 offset.height + (cy * brickHeight).roundToInt(),
                 brickWidthInt,
                 brickHeightInt,
             )
-            for (nextCoordIndex in 1..<snake.size) {
-                if (snake[nextCoordIndex].x == 0) {
-                    val yCache = snake[nextCoordIndex].y
+            for (nextCoordIndex in 1..<snake.pointList.size) {
+                if (snake.pointList[nextCoordIndex].x == 0) {
+                    val yCache = snake.pointList[nextCoordIndex].y
                     val yCacheAbsolute = yCache.absoluteValue
                     val yCacheSign = yCache.sign
                     for (j in 0..<yCacheAbsolute) {
@@ -127,7 +98,7 @@ class GameFieldPanel : JPanel() {
                         )
                     }
                 } else {
-                    val xCache = snake[nextCoordIndex].x
+                    val xCache = snake.pointList[nextCoordIndex].x
                     val xCacheAbsolute = xCache.absoluteValue
                     val xCacheSign = xCache.sign
                     for (i in 0..<xCacheAbsolute) {
@@ -149,17 +120,11 @@ class GameFieldPanel : JPanel() {
         }
     }
 
-//    fun updateField(newField: IntArray, newFieldWidth: Int, newFieldHeight: Int) {
-//        this.fieldWidth = newFieldWidth
-//        this.fieldHeight = newFieldHeight
-//        this.field = newField
-//        repaint()
-//    }
-
-    fun updateField(snakesKeyPointsList: List<List<KeyPoint>>, foodList: List<KeyPoint>, fieldWidth: Int, fieldHeight: Int) {
+    fun updateField(snakesList: List<Snake>, foodList: List<Coord>, myID: Int, fieldWidth: Int, fieldHeight: Int) {
         this.fieldWidth = fieldWidth
         this.fieldHeight = fieldHeight
-        this.snakesKeyPointsList = snakesKeyPointsList
+        this.snakesList = snakesList
+        this.myID = myID
         this.foodList = foodList
         repaint()
     }
