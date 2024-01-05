c48a6c67e146cda1a5047fd69419caf78d9467d7
fatalistix <slavabal11@outlook.com>
2024-01-05 17:47:41 +0700

    changed signature, added player name and server name passing

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ButtonsPanel.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ButtonsPanel.kt
@@ -1,13 +1,15 @@
 package ru.nsu.vbalashov2.onlinesnake.ui.impl.contentpanels
 
+import ru.nsu.vbalashov2.onlinesnake.dto.GameConfig
 import ru.nsu.vbalashov2.onlinesnake.ui.ExitListener
 import ru.nsu.vbalashov2.onlinesnake.ui.NewGameListener
-import ru.nsu.vbalashov2.onlinesnake.ui.dto.GameConfig
 import java.awt.GridLayout
 import javax.swing.JButton
 import javax.swing.JPanel
 
-class ButtonsPanel : JPanel(), ValidationFailListener, ValidationSuccessListener {
+class ButtonsPanel(
+    private val nicknameAndServerNameProperties: NicknameAndServerNameProperties,
+) : JPanel(), ValidationFailListener, ValidationSuccessListener {
     private val exitButton = JButton("Exit")
     private val newGameButton = JButton("New")
 
@@ -34,7 +36,11 @@ class ButtonsPanel : JPanel(), ValidationFailListener, ValidationSuccessListener
     init {
         newGameButton.addActionListener {
             newGameListenersList.forEach { listener ->
-                listener.newGame(verifiedGameConfig)
+                listener.newGame(
+                    verifiedGameConfig,
+                    nicknameAndServerNameProperties.serverName,
+                    nicknameAndServerNameProperties.nickname
+                )
             }
         }
     }
@@ -47,14 +53,12 @@ class ButtonsPanel : JPanel(), ValidationFailListener, ValidationSuccessListener
         }
     }
 
-    fun addNewGameListener(listener: NewGameListener): Int {
+    fun addNewGameListener(listener: NewGameListener) {
         this.newGameListenersList += listener
-        return this.newGameListenersList.size - 1
     }
 
-    fun addExitListener(listener: ExitListener): Int {
+    fun addExitListener(listener: ExitListener) {
         this.exitListenersList += listener
-        return this.exitListenersList.size - 1
     }
 
     override fun validationSuccess(gameConfig: GameConfig) {
@@ -65,35 +69,4 @@ class ButtonsPanel : JPanel(), ValidationFailListener, ValidationSuccessListener
     override fun validationFail() {
         newGameButton.isEnabled = false
     }
-
-
-//    init {
-//        this.layout = GridBagLayout()
-//    }
-//
-//    init {
-//        val gbcExitButton = GridBagConstraints()
-//        gbcExitButton.gridx = 0
-//        gbcExitButton.gridy = 0
-//        gbcExitButton.gridwidth = 1
-//        gbcExitButton.gridheight = 1
-//        gbcExitButton.fill = GridBagConstraints.BOTH
-//        gbcExitButton.anchor = GridBagConstraints.NORTHWEST
-//        gbcExitButton.weightx = 50.0
-//        gbcExitButton.weighty = 100.0
-//        this.add(exitButton, gbcExitButton)
-//    }
-//
-//    init {
-//        val gbcNewGameButton = GridBagConstraints()
-//        gbcNewGameButton.gridx = 1
-//        gbcNewGameButton.gridy = 0
-//        gbcNewGameButton.gridwidth = 1
-//        gbcNewGameButton.gridheight = 1
-//        gbcNewGameButton.fill = GridBagConstraints.BOTH
-//        gbcNewGameButton.anchor = GridBagConstraints.NORTHWEST
-//        gbcNewGameButton.weightx = 500.0
-//        gbcNewGameButton.weighty = 100.0
-//        this.add(newGameButton, gbcNewGameButton)
-//    }
 }
