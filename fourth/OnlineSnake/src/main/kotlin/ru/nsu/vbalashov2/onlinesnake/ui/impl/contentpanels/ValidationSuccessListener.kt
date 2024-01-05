1e9e99b19cb1a6b55be3a14adee990099266223d
fatalistix <slavabal11@outlook.com>
2024-01-05 17:50:32 +0700

    part of dto migration

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ValidationSuccessListener.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/ValidationSuccessListener.kt
@@ -1,6 +1,7 @@
 package ru.nsu.vbalashov2.onlinesnake.ui.impl.contentpanels
 
-import ru.nsu.vbalashov2.onlinesnake.ui.dto.GameConfig
+import ru.nsu.vbalashov2.onlinesnake.dto.GameConfig
+
 
 fun interface ValidationSuccessListener {
     fun validationSuccess(gameConfig: GameConfig)
