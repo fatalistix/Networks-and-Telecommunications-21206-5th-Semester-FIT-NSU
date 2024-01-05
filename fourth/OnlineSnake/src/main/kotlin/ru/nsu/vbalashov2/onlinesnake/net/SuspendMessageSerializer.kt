5205df733114f0561f0e3d7c8a1984dd917eb072
fatalistix <slavabal11@outlook.com>
2024-01-05 16:58:59 +0700

    Serializing interface for all Msg* classes to byte format according to protocol

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/SuspendMessageSerializer.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/SuspendMessageSerializer.kt
@@ -2,13 +2,14 @@ package ru.nsu.vbalashov2.onlinesnake.net
 
 import ru.nsu.vbalashov2.onlinesnake.net.dto.*
 
-interface SuspendMessageEnd {
-    suspend fun writeAnnouncement(msgAnnouncement: MsgAnnouncement)
-    suspend fun writePing(msgPing: MsgPing)
-    suspend fun writeSteer(msgSteer: MsgSteer)
-    suspend fun writeJoin(msgJoin: MsgJoin)
-    suspend fun writeRoleChange(msgRoleChange: MsgRoleChange)
-    suspend fun writeError(msgError: MsgError)
-    suspend fun writeAck(msgAck: MsgAck)
-    suspend fun writeState(msgState: MsgState)
+interface SuspendMessageSerializer {
+    suspend fun serializeAnnouncement(msgAnnouncement: MsgAnnouncement): ByteArray
+    suspend fun serializePing(msgPing: MsgPing): ByteArray
+    suspend fun serializeSteer(msgSteer: MsgSteer): ByteArray
+    suspend fun serializeJoin(msgJoin: MsgJoin): ByteArray
+    suspend fun serializeRoleChange(msgRoleChange: MsgRoleChange): ByteArray
+    suspend fun serializeError(msgError: MsgError): ByteArray
+    suspend fun serializeAck(msgAck: MsgAck): ByteArray
+    suspend fun serializeState(msgState: MsgState): ByteArray
+    suspend fun serializeDiscover(msgDiscover: MsgDiscover): ByteArray
 }
