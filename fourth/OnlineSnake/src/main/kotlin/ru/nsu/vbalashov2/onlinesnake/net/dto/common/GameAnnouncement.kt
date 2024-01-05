136ce622676ac03b76c911789e662ec0e25b9eb6
fatalistix <slavabal11@outlook.com>
2024-01-05 17:05:20 +0700

    part of dto migration from package local to common

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/dto/common/GameAnnouncement.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/net/dto/common/GameAnnouncement.kt
@@ -1,5 +1,7 @@
 package ru.nsu.vbalashov2.onlinesnake.net.dto.common
 
+import ru.nsu.vbalashov2.onlinesnake.dto.GameConfig
+
 data class GameAnnouncement(
     val playerList: List<Player>,
     val gameConfig: GameConfig,
