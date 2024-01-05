f9dc84a79624abe2d4bae75c75acddcd0f11b987
fatalistix <slavabal11@outlook.com>
2024-01-05 17:48:25 +0700

    changed signature, added game state stat showing

--- fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/CurrentGameInfoPanel.kt
+++ fourth/OnlineSnake/src/main/kotlin/ru/nsu/vbalashov2/onlinesnake/ui/impl/contentpanels/CurrentGameInfoPanel.kt
@@ -8,25 +8,30 @@ class CurrentGameInfoPanel : JPanel() {
     private val ownerPrefix = "Owner: "
     private val sizePrefix = "Field size: "
     private val foodPrefix = "Food on field: "
+    private val stateOrderPrefix = "State order: "
     private val ownerLabel = JLabel()
     private val sizeLabel = JLabel()
     private val foodLabel = JLabel()
+    private val stateOrderLabel = JLabel()
     init {
-        this.layout = GridLayout(1, 3)
+        this.layout = GridLayout(1, 4)
         this.add(ownerLabel)
         this.add(sizeLabel)
         this.add(foodLabel)
+        this.add(stateOrderLabel)
     }
 
-    fun updateCurrentGameInfo(fieldWidth: Int, fieldHeight: Int, foodOnField: Int, ownerName: String) {
+    fun updateCurrentGameInfo(fieldWidth: Int, fieldHeight: Int, foodOnField: Int, ownerName: String, stateOrder: Int) {
         ownerLabel.text = ownerPrefix + ownerName
         sizeLabel.text = sizePrefix + "${fieldWidth}x${fieldHeight}"
         foodLabel.text = foodPrefix + foodOnField
+        stateOrderLabel.text = stateOrderPrefix + stateOrder
     }
 
     fun clearCurrentGameInfo() {
         ownerLabel.text = ""
         sizeLabel.text = ""
         foodLabel.text = ""
+        stateOrderLabel.text = ""
     }
 }
